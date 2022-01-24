0c67102fbac13adad1f1e3b971c706883027044a
HBSPS <wiyeong79@naver.com>
2022-01-24 23:58:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,3 @@
-# """
-# These are the URLs that will give you remote jobs for the word 'python'
-
-# https://stackoverflow.com/jobs?r=true&q=python
-# https://weworkremotely.com/remote-jobs/search?term=python
-# https://remoteok.io/remote-dev+python-jobs
-
-# Good luck!
-# """
-
 from flask import Flask, render_template, request, redirect, send_file
 from SO_Scrapper import get_jobs_so
 from RO_Scrapper import get_jobs_ro
